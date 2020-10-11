9e72045a933d18720a8d6f1caba82f0b502925b5
sahil <sahil.a2111@gmail.com>
2020-10-11 21:29:46 +0530

    Update

--- practical_main/urls.py
+++ practical_main/urls.py
@@ -3,8 +3,7 @@ from . import views
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', views.userprofile, name='userprofile'),
-    # path('/register', views.register, name='register'),
-    path('register/', views.register, name='register'),
     path('display/', views.display, name='display'),
+    path('filter/', views.filter, name='filter'),
     path('delete/<int:pk>',views.delete, name='delete-person'),
 ]
--- practical_main/views.py
+++ practical_main/views.py
@@ -3,19 +3,6 @@ from django.contrib.auth.forms import UserCreationForm
 from .forms import UserprofileForm
 from .models import Userprofile
 # Create your views here.
-# def home(request):
-#     return render(request,'main_page.html')
-
-def register(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('')
-    else:
-        form = UserCreationForm()
-        return render(request, 'reg_forms.html', {"form":form})
-
 def userprofile(request):
     form = UserprofileForm(request.POST)
     if request.method == "POST":
@@ -33,14 +20,21 @@ def userprofile(request):
             'form': form
         }
         return render(request, 'main_page.html',context)
+
 def display(request):
     display = Userprofile.objects.all()
     for first in display:
         print(first.id)
     return render(request,'display.html',{'display':display})
+
 def delete(request,*args, **kwargs):
     id_grab = Userprofile
     id_grab = kwargs['pk']
     query = Userprofile.objects.get(id=id_grab)
     query.delete()
     return HttpResponse("Deleted!")
+
+def filter(request):
+    query_all = Userprofile.objects.all()
+    filter_lasttwo = query_all[::-2]
+    return render(request,'filter_lasttwo.html',{'filter_lasttwo':filter_lasttwo})
